18b5221c48749dc6f89574d9d09c1e49773ca2bd
Jerry Lum <serverofjerry@gmail.com>
2025-05-16 08:34:40 -0500

    Add V5RC Push Back

--- lib/competition/V5RC.py
+++ lib/competition/V5RC.py
@@ -2,13 +2,20 @@ import adsk.core
 import adsk.fusion
 from typing import Optional
 from ...lib import fusionAddInUtils as futil
+from ...lib.competition import V5RC_PushBack
 from ...lib.competition import V5RC_HighStakes
 from ...lib.competition import V5RC_OverUnder
 
 FIELD_PREFIX = "276-7596-000_With Tiles"
 
+
 def get_all_v5rc_game_object_prefixes() -> list[str]:
-    return [*V5RC_HighStakes.get_game_object_prefixes(), *V5RC_OverUnder.get_game_object_prefixes()]
+    return [
+        *V5RC_PushBack.get_game_object_prefixes(),
+        *V5RC_HighStakes.get_game_object_prefixes(),
+        *V5RC_OverUnder.get_game_object_prefixes(),
+    ]
+
 
 def get_V5RC_field(product: adsk.core.Product) -> Optional[adsk.fusion.Occurrence]:
     field_occurrence = None
---
+++ lib/competition/V5RC_PushBack.py
@@ -0,0 +1,5 @@
+BLOCK_PREFIX = "276-9142-001"
+
+
+def get_game_object_prefixes() -> list[str]:
+    return [BLOCK_PREFIX]
